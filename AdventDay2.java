3bb0cfc488a8659a626ba2c9efcb4e9c9fe51e8b
Deep Singh <sinriat@gmail.com>
2015-12-02 11:46:53 +0100

    Done

--- AdventDay2.java
+++ AdventDay2.java
@@ -1,11 +1,10 @@
 import java.io.File;
 import java.util.Scanner;
 
-public class Day2 {
+public class AdventDay2 {
 
     public static void main(String args[]) throws Exception{
 
-        // TASK 1
         Scanner scanFile = new Scanner(new File("paper.txt"));
 
         int paper_length = 0;
@@ -22,6 +21,7 @@ public class Day2 {
             width  = Integer.parseInt(temp[1]);
             height = Integer.parseInt(temp[2]);
 
+            // TASK 1
             paper_length += ((2 * length * width) + (2 * width * height) + (2 * height * length));
 
             if(length >= width && length >= height){
@@ -35,8 +35,10 @@ public class Day2 {
                 ribon_length += 2*width + 2*length;
             }
 
+            // TASK 2
             ribon_length += length * height * width;
         }
+        
         System.out.println(String.format("The required paper length: %d", paper_length));
         System.out.println(String.format("The required ribon length: %d", ribon_length));
     }
