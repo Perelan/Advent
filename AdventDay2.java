a308301875af9106430414d67fa61d5388be315f
Perelan <sinriat@gmail.com>
2015-12-06 20:29:23 +0100

    Update AdventDay2.java

--- AdventDay2.java
+++ AdventDay2.java
@@ -11,15 +11,12 @@ public class AdventDay2 {
         int ribon_length = 0;
 
         while(scanFile.hasNext()){
-            int length;
-            int width;
-            int height;
-
+            
             String[] temp = scanFile.next().split("x");
-
-            length = Integer.parseInt(temp[0]);
-            width  = Integer.parseInt(temp[1]);
-            height = Integer.parseInt(temp[2]);
+            
+            int length = Integer.parseInt(temp[0]);
+            int width  = Integer.parseInt(temp[1]);
+            int height = Integer.parseInt(temp[2]);
 
             // TASK 1
             paper_length += ((2 * length * width) + (2 * width * height) + (2 * height * length));
